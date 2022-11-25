e40f004029dfc0755689d4f099f268136286d4af
hankakukatakana <masaki.0409.a@gmail.com>
2022-11-26 05:33:30 +0900

    tokei

--- IntoroJava.java
+++ IntoroJava.java
@@ -1,13 +1,18 @@
 import java.util.Date;
+import java.util.Calendar;
 import java.time.LocalDateTime;
 
 class say{
     public static void main(String[] args){
+        Calendar CTime = Calendar.getInstance();
         Date DTime = new Date();
         LocalDateTime LTime = LocalDateTime.now();
-
+        
+        System.out.println(CTime);
         System.out.println(DTime);
         System.out.println(LTime);
+
+
         System.out.println("Good morning");
         System.out.println("Good afternoon");
         System.out.println("Good evening");
